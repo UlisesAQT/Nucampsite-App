18fef1b336edfa484972036c72ad1f5096e9b354
UlisesT <ulisestamayo1998@gmail.com>
2025-12-28 19:13:12 -0500

    component 3, added navigation and back button

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Main from './screens/MainComponent'
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <Main />
+    <NavigationContainer>
+      <Main />
+    </NavigationContainer>
   );
 }
 
