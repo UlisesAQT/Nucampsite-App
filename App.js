07799efc635e67265b4b3a660d795b4e8cea59b9
UlisesT <ulisestamayo1998@gmail.com>
2026-01-04 22:30:15 -0500

    Activity indicator

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import Main from './screens/MainComponent'
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 export default function App() {
   return (
+    <Provider store={store}>
     <NavigationContainer>
       <Main />
     </NavigationContainer>
+    </Provider>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#efe6e6ff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     color: 'blue' 
-//   },
-// });
