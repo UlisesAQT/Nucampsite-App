79f20316b71e2cc7e80b0f2d059f2eec1a892d9e
UlisesT <ulisestamayo1998@gmail.com>
2026-01-10 20:24:20 -0500

    added a new Directory navigator called favorites

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -16,6 +16,7 @@ import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import ReservationScreen from './ReservationScreen';
+import FavoritesScreen from './FavoritesScreen';
 
 
 
@@ -142,7 +143,28 @@ const ReservationNavigator = () =>{
     )
 }
 
-
+const FavoritesNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Favorites'
+                component={FavoritesScreen}
+                options={({ navigation }) => ({
+                    title: 'Favorite Campsites',
+                    headerLeft: () => (
+                        <Icon
+                            name='heart'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    );
+};
 
 const CustomDrawerContent = (props) => {
     return(
@@ -229,6 +251,25 @@ const Main = () => {
                     ),
                 }}
             />
+
+            <Drawer.Screen
+                name='FavoritesNav'
+                component={FavoritesNavigator}
+                options={{
+                    title: 'My Favorites',
+                    headerShown: false,
+                    drawerIcon: ({ color }) => (
+                        <Icon
+                            name='heart'
+                            type='font-awesome'
+                            size={24}
+                            iconStyle={{ width: 24 }}
+                            color={color}
+                        />
+                    )
+                }}
+            />
+
 
              <Drawer.Screen 
             name='AboutNav'
